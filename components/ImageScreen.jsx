0fda9335b9a5014ba836f8023febeebe2627189c
vipulpathak113 <vipulpathak113@gmail.com>
2023-03-27 19:23:00 +0530

    hooks

--- components/ImageScreen.jsx
+++ components/ImageScreen.jsx
@@ -1,21 +1,47 @@
-import { Text, ScrollView, StyleSheet, Image, View } from "react-native";
+import {
+  Text,
+  ScrollView,
+  StyleSheet,
+  Image,
+  View,
+  useColorScheme,
+} from "react-native";
 import React from "react";
 
 export default function ImageScreen() {
+  const colorScheme = useColorScheme();
   return (
-    <ScrollView indicatorStyle={"white"}>
+    <ScrollView
+      style={[
+        colorScheme === "light"
+          ? { backgroundColor: "#fff" }
+          : { backgroundColor: "#333333" },
+      ]}
+      indicatorStyle={"white"}
+    >
       <View style={imageStyle.imageView}>
         <Image
           style={imageStyle.image}
           source={require("../img/logo.png")}
           resizeMode="cover"
-          accessible= {true}
+          accessible={true}
           accessibilityLabel="Little lemon logo"
-                     
         />
-        <Text style={imageStyle.headingText}>Little Lemon</Text>
+        <Text
+          style={[
+            imageStyle.headingText,
+            colorScheme === "light" ? "black" : "#EDEFEE",
+          ]}
+        >
+          Little Lemon
+        </Text>
       </View>
-      <Text style={imageStyle.mainText}>
+      <Text
+        style={[
+          imageStyle.mainText,
+          colorScheme === "light" ? "black" : "#EDEFEE",
+        ]}
+      >
         Little Lemon is a charming neighborhood bistro that serves simple food
         and classic cocktails in a lively but casual environment. We would love
         to hear more about your experience with us!
@@ -28,12 +54,12 @@ const imageStyle = StyleSheet.create({
   image: {
     width: 100,
     height: 100,
-    borderRadius:20
+    borderRadius: 20,
   },
-  imageView:{
+  imageView: {
     flexDirection: "row",
-    margin:10,
-    justifyContent:"center",
+    margin: 10,
+    justifyContent: "center",
   },
   headingText: {
     paddingRight: 10,
@@ -41,14 +67,12 @@ const imageStyle = StyleSheet.create({
     paddingTop: 30,
     paddingBottom: 10,
     fontSize: 30,
-    color: '#EDEFEE',
-    textAlign: 'center',
+    textAlign: "center",
   },
   mainText: {
     fontSize: 24,
     padding: 20,
     marginVertical: 8,
-    color: "#EDEFEE",
     textAlign: "center",
   },
 });
