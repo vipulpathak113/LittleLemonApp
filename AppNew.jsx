6bda780e8a6e4092b6c1f025eb8f361ec46d8389
Vipul Pathak <vipulpathak113@gmail.com>
2023-03-31 12:50:48 +0530

    splash and icon added

--- AppNew.jsx
+++ AppNew.jsx
@@ -1,14 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import RootNavigator from './navigators/RootNavigator'
-import { NavigationContainer } from '@react-navigation/native'
+import { View, Text } from "react-native";
+import React from "react";
+import RootNavigator from "./navigators/RootNavigator";
+import { NavigationContainer } from "@react-navigation/native";
+import RNBootSplash from "react-native-bootsplash";
 
 export default function AppNew() {
   return (
-    <NavigationContainer>
-    <View style={{flex:1}}>
-      <RootNavigator/>
-    </View>
+    <NavigationContainer onReady={() => RNBootSplash.hide()}>
+      <View style={{ flex: 1 }}>
+        <RootNavigator />
+      </View>
     </NavigationContainer>
-  )
+  );
 }
